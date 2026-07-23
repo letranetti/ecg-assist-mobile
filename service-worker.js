d5488a7058a02e7c6dba2d868c70cebd8c0026f2
letranetti <letranetti@gmail.com>
2026-07-23 13:35:53 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C="ecg-v4";const F=["./","./index.html","./styles.css","./app.js","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png","./icons/maskable-512.png"];self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
